5baf9c08e242f883abf47ef278aa4e0c398426a6
David Zhang <davidzhang.seu@gmail.com>
2013-01-30 14:35:48 -0500

    add longestPalindrom problem

---
+++ LongestPalindromic.java
@@ -0,0 +1,13 @@
+// Given a string S, find the longest palindromic substring in S
+// http://www.leetcode.com/2011/11/longest-palindromic-substring-part-i.html
+
+public class LongestPalindromic {
+    public static void main(String[] args) {
+        String s = "";
+        longestPalindromeDP(s);
+    }
+
+    public static String longestPalindromeDP(String s) {
+        
+    }
+}
