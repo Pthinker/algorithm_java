3990780b9d79e7daa75c37e972dd0ccae5553e7f
Wei Zhang <davidzhang.seu@gmail.com>
2017-02-12 18:50:12 -0800

    some hard problems

--- leetcode/Median_of_Two_Sorted_Arrays.java
+++ leetcode/Median_of_Two_Sorted_Arrays.java
@@ -1,12 +1,23 @@
-There are two sorted arrays A and B of size m and n respectively. Find the median of 
-the two sorted arrays. The overall run time complexity should be O(log(m+n)).
+4. Median of Two Sorted Arrays
+
+There are two sorted arrays nums1 and nums2 of size m and n respectively.
+Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+
+Example 1:
+nums1 = [1, 3]
+nums2 = [2]
+
+The median is 2.0
+Example 2:
+nums1 = [1, 2]
+nums2 = [3, 4]
+
+The median is (2 + 3)/2 = 2.5
 
 
 // O(m+n)
 public class Solution {
     public double findMedianSortedArrays(int A[], int B[]) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
         int[] tmp = new int[A.length + B.length];
         int i = 0;
         int j = 0;
@@ -34,54 +45,38 @@ public class Solution {
 
 ////////////////////////////////////////////////////////////////////////////////
 
+//O(log(min(m,n)))
+//https://segmentfault.com/a/1190000002988010
 public class Solution {
-    public double findMedianSortedArrays(int A[], int B[]) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        int m = A.length;
-        int n = B.length;
-        int median1 = (m + n) / 2 + 1;
-        int median2 = (m + n - 1) / 2 + 1;
-        return (findKth(A, 0, m, B, 0, n, median1) + findKth(A, 0, m, B, 0, n,
-                median2)) / 2f;
-    }
-
-    private int findKth(int a[], int as, int ae, int b[], int bs, int be, int k) {
-        int m = ae - as;
-        int n = be - bs;
-        if (m == 0) {
-            return b[bs + k - 1];
-        } else if (n == 0) {
-            return a[as + k - 1];
-        }
-        if (m == 1 && n == 1) {
-            if (k == 1) {
-                return Math.min(a[as], b[bs]);
-            } else {
-                return Math.max(a[as], b[bs]);
-            }
-        }
-        if (k == m + n) {
-            return Math.max(a[ae - 1], b[be - 1]);
-        }
-        float ratio = k * m * 1f / (m + n);
-        int i = Math.round(m * ratio);
-        i = fit(i, 0, m - 1);
-        i = fit(i, k - 1 - n, k - 1);
-        int j = k - 1 - i;
-        if (a[as + i] > b[bs + j]) {
-            return findKth(a, as, as + i, b, bs + j, be, k - j);
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length, n = nums2.length;
+        int k = (m + n) / 2;
+        if((m+n)%2==0){
+            return (findKth(nums1,nums2,0,0,m,n,k)+findKth(nums1,nums2,0,0,m,n,k+1))/2;
         } else {
-            return findKth(a, as + i, ae, b, bs, bs + j, k - i);
+            return findKth(nums1,nums2,0,0,m,n,k+1);
         }
     }
-
-    private int fit(int a, int lower, int upper) {
-        if (a < lower)
-            return lower;
-        else if (a > upper)
-            return upper;
-        else
-            return a;
+    
+    private double findKth(int[] arr1, int[] arr2, int start1, int start2, int len1, int len2, int k){
+        // 保证arr1是较短的数组
+        if(len1>len2){
+            return findKth(arr2,arr1,start2,start1,len2,len1,k);
+        }
+        if(len1==0){
+            return arr2[start2 + k - 1];
+        }
+        if(k==1){
+            return Math.min(arr1[start1],arr2[start2]);
+        }
+        int p1 = Math.min(k/2,len1) ;
+        int p2 = k - p1;
+        if(arr1[start1 + p1-1]<arr2[start2 + p2-1]){
+            return findKth(arr1,arr2,start1 + p1,start2,len1-p1,len2,k-p1);
+        } else if(arr1[start1 + p1-1]>arr2[start2 + p2-1]){
+            return findKth(arr1,arr2,start1,start2 + p2,len1,len2-p2,k-p2);
+        } else {
+            return arr1[start1 + p1-1];
+        }
     }
 }
